6f669b8ad5dfcc10bbc6c53ba9d05239d5cfcfce
Fernando Nagase <nagase.fernando@gmail.com>
2025-07-12 17:51:34 -0300

    Criar serviço SqfliteClient

---
+++ lib/data/services/sqflite_client.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:ngsfer_wishlist/data/dtos/item_dto.dart';
+import 'package:result_dart/result_dart.dart';
+import 'package:sqflite/sqflite.dart';
+
+const dbName = 'ngsfer_wishlist.db';
+
+class SqfliteClient {
+  static final SqfliteClient _sqfliteClient = SqfliteClient._internal();
+
+  SqfliteClient._internal();
+  factory SqfliteClient() => _sqfliteClient;
+
+  // Não consumir _dbConnection diretamente. Em vez disso, obter conexão
+  // a partir de _getDbConnection, que garante que a conexão foi inicializada.
+  Database? _dbConnection;
+
+  _onCreate(Database db, int version) async {
+    await db.execute(
+      'CREATE TABLE item (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, notes TEXT, price INTEGER);',
+    );
+  }
+
+  Future<void> _init() async {
+    try {
+      await Directory(await getDatabasesPath()).create(recursive: true);
+    } catch (_) {}
+    _dbConnection = await openDatabase(
+      dbName,
+      version: 1,
+      onCreate: _onCreate,
+      onDowngrade: onDatabaseDowngradeDelete,
+      singleInstance: true,
+    );
+  }
+
+  Future<Database> _getDbConnection() async {
+    if (_dbConnection == null) {
+      await _init();
+    }
+    return _dbConnection!;
+  }
+
+  Future<Result<List<ItemDto>>> getItems() async {
+    try {
+      final itemsMap = await (await _getDbConnection()).query(
+        'item',
+        columns: ['id', 'name', 'notes', 'price'],
+      );
+      return Success(
+        itemsMap.map((itemMap) => ItemDto.fromMap(itemMap)).toList(),
+      );
+    } on Exception catch (e) {
+      return Failure(e);
+    }
+  }
+}
