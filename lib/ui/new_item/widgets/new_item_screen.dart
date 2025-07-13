7885e146cf50553bb76a1206edb7d9cb83507ff4
Fernando Nagase <nagase.fernando@gmail.com>
2025-07-13 14:15:32 -0300

    Desabilitar formulário durante envio

--- lib/ui/new_item/widgets/new_item_screen.dart
+++ lib/ui/new_item/widgets/new_item_screen.dart
@@ -30,101 +30,117 @@ class _NewItemScreenState extends State<NewItemScreen> {
   Widget build(BuildContext context) {
     final viewModel = widget.viewModel;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Adicionar item'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () async {
-              if (_formKey.currentState!.validate()) {
-                await viewModel.addItemCommand.execute((
-                  name: _descricaoController.text,
-                  notes: _observacoesController.text,
-                  price: _precoController.text,
-                ));
-                context.pop();
-              }
-            },
-            child: const Text('Confirmar'),
-          ),
-        ],
-      ),
-      body: ListenableBuilder(
-        listenable: widget.viewModel.addItemCommand,
-        builder: (context, _) {
-          return Column(
-            children: [
-              AnimatedSwitcher(
-                duration: const Duration(milliseconds: 200),
-                child: widget.viewModel.addItemCommand.value.isRunning
-                    ? const LinearProgressIndicator(key: ValueKey('progress'))
-                    : const SizedBox(key: ValueKey('empty'), height: 4),
-              ),
-              SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      spacing: 12.0,
-                      children: [
-                        Row(
-                          spacing: 12.0,
-                          children: [
-                            Expanded(
-                              flex: 4,
-                              child: TextFormField(
-                                controller: _descricaoController,
-                                textInputAction: TextInputAction.next,
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'Descrição*',
-                                  helperText: '',
-                                ),
-                                autofocus: true,
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Campo obrigatório.';
-                                  }
-                                  return null;
-                                },
+    return ListenableBuilder(
+      listenable: widget.viewModel.addItemCommand,
+      builder: (context, _) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Adicionar item'),
+          leading: widget.viewModel.addItemCommand.value.isRunning
+              ? Container() // Oculta a seta de voltar
+              : null,
+          actions: <Widget>[
+            TextButton(
+              onPressed: widget.viewModel.addItemCommand.value.isRunning
+                  ? null
+                  : () async {
+                      if (_formKey.currentState!.validate()) {
+                        await viewModel.addItemCommand.execute((
+                          name: _descricaoController.text,
+                          notes: _observacoesController.text,
+                          price: _precoController.text,
+                        ));
+                        if (!context.mounted) return;
+                        context.pop();
+                      }
+                    },
+              child: const Text('Confirmar'),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 200),
+              child: widget.viewModel.addItemCommand.value.isRunning
+                  ? const LinearProgressIndicator(key: ValueKey('progress'))
+                  : const SizedBox(key: ValueKey('empty'), height: 4),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    spacing: 12.0,
+                    children: [
+                      Row(
+                        spacing: 12.0,
+                        children: [
+                          Expanded(
+                            flex: 4,
+                            child: TextFormField(
+                              controller: _descricaoController,
+                              textInputAction: TextInputAction.next,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Descrição*',
+                                helperText: '',
                               ),
+                              autofocus: true,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Campo obrigatório.';
+                                }
+                                return null;
+                              },
+                              readOnly: widget
+                                  .viewModel
+                                  .addItemCommand
+                                  .value
+                                  .isRunning,
                             ),
-                            Expanded(
-                              flex: 2,
-                              child: TextFormField(
-                                controller: _precoController,
-                                textInputAction: TextInputAction.next,
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'Preço',
-                                  prefixText: 'R\$ ',
-                                  helperText: '',
-                                ),
-                                keyboardType: TextInputType.number,
-                              ),
-                            ),
-                          ],
-                        ),
-                        TextFormField(
-                          controller: _observacoesController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Observações',
-                            alignLabelWithHint: true,
                           ),
-                          keyboardType: TextInputType.multiline,
-                          minLines: 3,
-                          maxLines: null,
+                          Expanded(
+                            flex: 2,
+                            child: TextFormField(
+                              controller: _precoController,
+                              textInputAction: TextInputAction.next,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Preço',
+                                prefixText: 'R\$ ',
+                                helperText: '',
+                              ),
+                              keyboardType: TextInputType.number,
+                              readOnly: widget
+                                  .viewModel
+                                  .addItemCommand
+                                  .value
+                                  .isRunning,
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextFormField(
+                        controller: _observacoesController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Observações',
+                          alignLabelWithHint: true,
                         ),
-                      ],
-                    ),
+                        keyboardType: TextInputType.multiline,
+                        minLines: 3,
+                        maxLines: null,
+                        readOnly:
+                            widget.viewModel.addItemCommand.value.isRunning,
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ],
-          );
-        },
+            ),
+          ],
+        ),
       ),
     );
   }
